87165331c66d8613680ef1a2baece1686b4d6648
meandevstar <andrew.norasing@gmail.com>
2023-01-19 04:58:44 -0500

    :lipstick: add header

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import CssBaseline from '@mui/material/CssBaseline';
 
 // Components
 import AppRoutes from './Routes';
+import Header from './components/Header';
 
 function App() {
   return (
     <div>
+      <Header />
       <AppRoutes />
       <CssBaseline />
       <GlobalStyles styles={{ body: { backgroundColor: '#f6f6f6' } }} />
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,36 @@
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Container from '@mui/material/Container';
+
+// Components and Styles
+import { Button } from 'components/common';
+
+// Hooks
+import { useAppSelector } from 'hooks';
+
+// Store
+import { selectSessionUser } from 'store/session/selectors';
+
+const Header = () => {
+  const user = useAppSelector(selectSessionUser);
+
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            <Button to="/survivors">
+              Survivors
+            </Button>
+            <Button to="/reports">
+              Reports
+            </Button>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+
+export default Header;
