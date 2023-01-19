fd595b33c912c7174a550958157e8bb7be5e6f25
meandevstar <andrew.norasing@gmail.com>
2023-01-19 04:44:58 -0500

    :lipstick: add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
+import GlobalStyles from '@mui/material/GlobalStyles';
+import CssBaseline from '@mui/material/CssBaseline';
 function App() {
   return (
     <div>
+      <CssBaseline />
+      <GlobalStyles styles={{ body: { backgroundColor: '#f6f6f6' } }} />
     </div>
   );
 }
