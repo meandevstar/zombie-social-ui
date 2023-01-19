d7e5a0f73fce992329cfb6fb1a8a732b6dcaf78b
meandevstar <andrew.norasing@gmail.com>
2023-01-19 05:06:02 -0500

    :sparkles: fetch user and survivors on app init

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+
 import GlobalStyles from '@mui/material/GlobalStyles';
 import CssBaseline from '@mui/material/CssBaseline';
 
@@ -5,7 +7,26 @@ import CssBaseline from '@mui/material/CssBaseline';
 import AppRoutes from './Routes';
 import Header from './components/Header';
 
+// Hooks
+import { useAppDispatch, useAppSelector } from 'hooks';
+
+// Store
+import { getSurvivors, getUser } from 'store/session';
+import { selectSessionUser } from 'store/session/selectors';
+
 function App() {
+  const dispatch = useAppDispatch();
+
+  const user = useAppSelector(selectSessionUser);
+
+  useEffect(() => {
+    dispatch(getSurvivors());
+    if (user) {
+      dispatch(getUser(user.id));
+    }
+    // eslint-disable-next-line
+  }, [dispatch]);
+
   return (
     <div>
       <Header />
