d3152c39f9299a48764202a31f689e4fafdf092a
meandevstar <andrew.norasing@gmail.com>
2023-01-19 04:57:49 -0500

    :lipstick: add account page

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 // Pages
+import Account from 'pages/Account';
 import Reports from 'pages/Reports';
 import Survivors from 'pages/Survivors';
 import Register from 'pages/Register';
@@ -11,6 +12,7 @@ const AppRoutes = () => {
       <Route path="/survivors" element={<Survivors />} />
       <Route path="/reports" element={<Reports />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/account" element={<Account />} />
       <Route path="/" element={<Navigate to="/survivors" replace />} />
     </Routes>
   )
---
+++ src/pages/Account/Account.tsx
@@ -0,0 +1,9 @@
+const Account = () => {
+  return (
+    <Container>
+      <Typography variant="h2" align="center" marginTop="30px">Last location</Typography>
+    </Container>
+  )
+}
+
+export default Account;
