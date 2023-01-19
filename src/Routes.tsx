287347f30c5e1b3b8954d291c91853c8a767db3b
meandevstar <andrew.norasing@gmail.com>
2023-01-19 04:56:21 -0500

    :lipstick: add register page

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -3,12 +3,14 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 // Pages
 import Reports from 'pages/Reports';
 import Survivors from 'pages/Survivors';
+import Register from 'pages/Register';
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/survivors" element={<Survivors />} />
       <Route path="/reports" element={<Reports />} />
+      <Route path="/register" element={<Register />} />
       <Route path="/" element={<Navigate to="/survivors" replace />} />
     </Routes>
   )
