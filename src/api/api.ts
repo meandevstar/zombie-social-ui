7ad949da10c1d03a7cb7b4cdd8479114cf2c219c
meandevstar <andrew.norasing@gmail.com>
2023-01-20 07:07:00 -0500

    :sparkles: add statistics endpoint, add pagination to survivors endpoint

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,10 +1,21 @@
 import type { AxiosResponse } from 'axios';
-import type { CreateUser, Trade, UpdateUser, User, UserReport } from 'definitions/interfaces';
+import type {
+  CreateUser,
+  Trade,
+  UpdateUser,
+  User,
+  UserReport,
+  Statistics,
+  PaginationParams,
+  Pagination,
+} from 'definitions/interfaces';
 
 import { api } from './api-base';
 
-export const getSurvivors = (): Promise<AxiosResponse<User[]>> =>
-  api.get('/survivors');
+export const getSurvivors = (params: PaginationParams): Promise<AxiosResponse<Pagination<User>>> =>
+  api.get('/survivors', {
+    params,
+  });
 export const getSurvivorById = (id: string): Promise<AxiosResponse<User>> =>
   api.get(`/survivors/${id}`);
 export const updateSurvivor = ({ id, data }: UpdateUser): Promise<AxiosResponse<null>> =>
@@ -18,3 +29,5 @@ export const register = (params: CreateUser): Promise<AxiosResponse<User>> =>
   api.post('/survivors', params);
 export const trade = (data: Trade): Promise<AxiosResponse> =>
   api.post('/trades', data);
+export const getStatistics = (): Promise<AxiosResponse<Statistics>> =>
+  api.get('/statistics');
